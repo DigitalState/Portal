9a0d0d8ca2536076d34c6b1c8fe5e6504acc4777
Bushra Addlan <bushra.addlan@gmail.com>
2017-07-27 09:55:10 -0400

    Removed footer from the main page template and added host component classes.

--- ng2-admin/src/app/pages/pages.component.ts
+++ ng2-admin/src/app/pages/pages.component.ts
@@ -15,7 +15,7 @@ import { PAGES_MENU } from './pages.menu';
         <router-outlet></router-outlet>
       </div>
     </div>
-    <footer class="al-footer clearfix">
+    <!--<footer class="al-footer clearfix">-->
       <!--<div class="al-footer-right" translate>{{'general.created_with'}} <i class="ion-heart"></i></div>-->
       <!--<div class="al-footer-main clearfix">-->
         <!--<div class="al-copy">&copy; <a href="http://akveo.com" translate>{{'general.akveo'}}</a> 2016</div>-->
@@ -26,9 +26,13 @@ import { PAGES_MENU } from './pages.menu';
           <!--<li><i class="socicon socicon-github"></i></li>-->
         <!--</ul>-->
       <!--</div>-->
-    </footer>
+    <!--</footer>-->
     <ba-back-top position="200"></ba-back-top>
-    `
+    `,
+  host: {
+    id: 'pages',
+    class: 'd-block w-100',
+  }
 })
 export class Pages {
 
