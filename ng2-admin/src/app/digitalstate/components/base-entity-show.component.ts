d485a6f09afb7283a69b96a0f5a4effc6b724961
Bushra Addlan <bushra.addlan@gmail.com>
2017-09-25 18:15:19 -0400

    Updated the template language variable `lang` when the UI language changes in base entity `show` component.

--- ng2-admin/src/app/digitalstate/components/base-entity-show.component.ts
+++ ng2-admin/src/app/digitalstate/components/base-entity-show.component.ts
@@ -71,6 +71,7 @@ export abstract class DsBaseEntityShowComponent extends DsEntityCrudComponent {
 
         // Subscribe to language-change events
         this.languageChangeSubscriber = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+            this.lang = event.lang;
             this.prepareEntity().subscribe();
             this.updateTranslations(event.lang);
         });
