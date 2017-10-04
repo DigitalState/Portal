b9d5a9567f5975bc638ee4ea303a4b5ec920dd46
Bushra Addlan <bushra.addlan@gmail.com>
2017-10-04 14:02:48 -0400

    - Updated tasks `list` and `show` components to use the new Formio modal IFrame for task activation. - Added an `info` button that links to the `show` component and had the list item perform the activation.

--- ng2-admin/src/app/digitalstate/modules/task/components/task-list.component.ts
+++ ng2-admin/src/app/digitalstate/modules/task/components/task-list.component.ts
@@ -1,25 +1,40 @@
 import { Component, Injector } from '@angular/core';
-import 'rxjs/Rx';
+
+import { NgbModal, NgbModalOptions, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+
+import { MicroserviceConfig } from '../../../../shared/providers/microservice.provider';
+import { FormioApiService } from '../../../../shared/services/formio-api.service';
+import { FormioController } from "../../../../shared/components/modals/formio-controller";
+import { FormioModalFrameComponent } from "../../../../shared/components/modals/formio-modal-frame.component";
 
 import { EntityApiService } from '../entity-api.service';
 import { DsBaseEntityListComponent } from '../../../components/base-list.component';
-import { MicroserviceConfig } from '../../../../shared/providers/microservice.provider';
+
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'ds-task-list',
     templateUrl: '../templates/task-list.template.html'
 })
-export class DsTaskListComponent extends DsBaseEntityListComponent {
+export class DsTaskListComponent extends DsBaseEntityListComponent implements FormioController {
 
     entityUrlPrefix = 'tasks';
     pageTitle = 'general.menu.tasks';
 
+    formioModal: NgbModalRef;
+    iFrameModalComponent: FormioModalFrameComponent;
+    selectedEntityUuid: string; // set upon clicking on a row to activate it
+
     constructor(injector: Injector,
                 microserviceConfig: MicroserviceConfig,
-                entityApiService: EntityApiService) {
+                entityApiService: EntityApiService,
+                protected formioApiService: FormioApiService,
+                protected modal: NgbModal) {
 
         super(injector, microserviceConfig);
         this.entityApiService = entityApiService;
+        this.formioApiService.setEntityApiService(entityApiService);
     }
 
     /**
@@ -41,4 +56,39 @@ export class DsTaskListComponent extends DsBaseEntityListComponent {
 
         return rows;
     }
+
+    // // // Formio // // // // // // // // // // // // // // // // // // // // // // //
+
+    protected activateFormioForm(entity) {
+        this.selectedEntityUuid = entity.uuid;
+        this.openModalIFrame();
+    }
+
+    protected openModalIFrame() {
+        const modalOptions: NgbModalOptions = {
+            size: 'lg',
+            windowClass: 'formio-modal-frame',
+        };
+
+        this.formioModal = this.modal.open(FormioModalFrameComponent, modalOptions);
+        this.iFrameModalComponent = this.formioModal.componentInstance;
+        this.iFrameModalComponent.setFormioController(this);
+    }
+
+    requestFormioForm(): Observable<any> {
+        return this.formioApiService.getForm('tasks', this.selectedEntityUuid);
+    }
+
+    submitFormioForm(formData: any): Observable<any> {
+        return this.formioApiService.submitFormUsingPut('tasks', this.selectedEntityUuid, formData, 'submission').flatMap(submissionResult => {
+            this.formioModal.close();
+            this.toastr.success(this.translate.instant('ds.microservices.entity.task.submissionSuccess'));
+            return Observable.of(submissionResult);
+        });
+    }
+
+    handleFormioFormEvent(lifeCycleMethod: string, arg: any) {
+        // Do nothing
+    }
+
 }
--- ng2-admin/src/app/digitalstate/modules/task/components/task-show.component.ts
+++ ng2-admin/src/app/digitalstate/modules/task/components/task-show.component.ts
@@ -1,30 +1,76 @@
 import { Component, Injector } from '@angular/core';
 
-import { DsBaseEntityShowComponent } from '../../../components/base-entity-show.component';
+import { NgbModalOptions, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+
 import { MicroserviceConfig } from '../../../../shared/providers/microservice.provider';
+import { FormioApiService } from '../../../../shared/services/formio-api.service';
+import { FormioController } from "../../../../shared/components/modals/formio-controller";
+import { FormioModalFrameComponent } from "../../../../shared/components/modals/formio-modal-frame.component";
+
+import { DsBaseEntityShowComponent } from '../../../components/base-entity-show.component';
 import { EntityApiService } from '../entity-api.service';
 import { Link } from '../../../models/link';
 
 import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'ds-task-show',
     templateUrl: '../templates/task-show.template.html'
 })
-export class DsTaskShowComponent extends DsBaseEntityShowComponent {
+export class DsTaskShowComponent extends DsBaseEntityShowComponent implements FormioController {
 
     entityUrlPrefix = 'tasks';
 
+    formioModal: NgbModalRef;
+    iFrameModalComponent: FormioModalFrameComponent;
+
     constructor(injector: Injector,
                 microserviceConfig: MicroserviceConfig,
-                entityApiService: EntityApiService) {
-        
+                entityApiService: EntityApiService,
+                protected formioApiService: FormioApiService) {
+
         super(injector, microserviceConfig);
         this.entityApiService = entityApiService;
+        this.formioApiService.setEntityApiService(entityApiService);
     }
 
     ngOnInit() {
         super.ngOnInit();
         this.backLink = new Link(['/pages/tasks'], 'general.menu.tasks');
+    }
+
+
+    // // // Formio // // // // // // // // // // // // // // // // // // // // // // // //
+
+    protected activateFormioForm() {
+        this.openModalIFrame();
+    }
+
+    protected openModalIFrame() {
+        const modalOptions: NgbModalOptions = {
+            size: 'lg',
+            windowClass: 'formio-modal-frame',
+        };
+
+        this.formioModal = this.modal.open(FormioModalFrameComponent, modalOptions);
+        this.iFrameModalComponent = this.formioModal.componentInstance;
+        this.iFrameModalComponent.setFormioController(this);
+    }
+
+    requestFormioForm(): Observable<any> {
+        return this.formioApiService.getForm('tasks', this.entity.uuid);
+    }
+
+    submitFormioForm(formData: any): Observable<any> {
+        return this.formioApiService.submitFormUsingPost('tasks', this.entity.uuid, formData).flatMap(submissionResult => {
+            this.formioModal.close();
+            this.toastr.success(this.translate.instant('ds.microservices.entity.task.submissionSuccess'));
+            return Observable.of(submissionResult);
+        });
+    }
+
+    handleFormioFormEvent(lifeCycleMethod: string, arg: any) {
+        // Do nothing
     }
 }
