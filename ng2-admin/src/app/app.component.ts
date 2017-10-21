a461a1b5b320299e8580ae4efaa5524537e99f57
Bushra Addlan <bushra.addlan@gmail.com>
2017-10-21 00:48:33 -0400

    Added initial CMS content to the pre-loading sequence in the AppComponent.

--- ng2-admin/src/app/app.component.ts
+++ ng2-admin/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, ViewContainerRef } from '@angular/core';
 
 import { GlobalState } from './global.state';
+import { AppState } from './app.service';
 import { BaImageLoaderService, BaThemePreloader, BaThemeSpinner } from './theme/services';
 import { BaThemeConfig } from './theme/theme.config';
 import { layoutPaths } from './theme/theme.constants';
 
-import { ToastsManager, Toast } from 'ng2-toastr';
+import { ToastsManager, Toast } from 'ng2-toastr'
+
+import { CmsApiService } from './shared/services/cms.service';
+
+import { Observable } from 'rxjs/Observable';
 
 import 'style-loader!./app.scss';
 import 'style-loader!./theme/initial.scss';
@@ -29,21 +34,22 @@ export class App {
   isMenuCollapsed: boolean = false;
 
   constructor(private _state: GlobalState,
+              private appState: AppState,
               private _imageLoader: BaImageLoaderService,
               private _spinner: BaThemeSpinner,
               private viewContainerRef: ViewContainerRef,
               private themeConfig: BaThemeConfig,
-              private toastr: ToastsManager) {
+              private toastr: ToastsManager,
+              private cms: CmsApiService) {
 
-    this.toastr.setRootViewContainerRef(viewContainerRef);
+      this.toastr.setRootViewContainerRef(viewContainerRef);
+      themeConfig.config();
+      this.loadImages();
+      this.loadContent();
 
-    themeConfig.config();
-
-    this._loadImages();
-
-    this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
-      this.isMenuCollapsed = isCollapsed;
-    });
+      this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+          this.isMenuCollapsed = isCollapsed;
+      });
   }
 
   public ngAfterViewInit(): void {
@@ -53,9 +59,24 @@ export class App {
     });
   }
 
-  private _loadImages(): void {
+  private loadImages(): void {
     // register some loaders
-    BaThemePreloader.registerLoader(this._imageLoader.load(layoutPaths.images.root + 'sky-bg.jpg'));
+    // BaThemePreloader.registerLoader(this._imageLoader.load(layoutPaths.images.root + 'sky-bg.jpg'));
   }
 
+    private loadContent(): void {
+        const contentSlugs = {
+            'texts[]': ['portal-title']
+        };
+
+        let loaderPromise: Promise<any> = this.cms.getContentBySlugs(contentSlugs).flatMap(content => {
+            this.appState.set('appCmsContent', content);
+            console.log('AppState in loadContent', this.appState);
+            return Observable.of(true);
+        }).toPromise().catch((rejectionReason: any) => {
+            console.warn('Unable to load initial SPA content from CMS. Reason: ', rejectionReason);
+        });
+
+        BaThemePreloader.registerLoader(loaderPromise);
+    }
 }
